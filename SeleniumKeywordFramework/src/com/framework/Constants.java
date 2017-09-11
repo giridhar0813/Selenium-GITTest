bf0c6a426eee5d94d55486a8771909aab102edc3
giridhar <giridhar_m13@yahoo.com>
2017-09-11 13:12:10 +0530

    first commit

---
+++ SeleniumKeywordFramework/src/com/framework/Constants.java
@@ -0,0 +1,9 @@
+package com.framework;
+
+public class Constants {
+	
+	final static String Master_Data_Sheet ="C:\\Users\\giridhar\\workspace\\SeleniumKeywordFramework\\Report-Output\\readexcel.xlsx";
+	final static String Report_Data_Sheet ="C:\\Users\\giridhar\\Desktop\\tmp\\ReportExcel.xlsx";
+	
+
+}
